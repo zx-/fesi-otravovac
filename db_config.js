680ff86b72e6b0600d5c3b2e4eabe7463cc8868f
zx- <robertcuprik@hotmail.com>
2015-12-19 01:11:40 +0100

    prepares for db

---
+++ db_config.js
@@ -0,0 +1,4 @@
+/**
+ * Created by z on 19.12.2015.
+ */
+module.exports.conString = "";
--- main.js
+++ main.js
@@ -2,7 +2,8 @@
  * Created by z on 18.12.2015.
  */
 
-var FESIO = require ('./configuration.js');
+var FESIO = require('./configuration.js');
+var DB_CFG = require('./db_config.js');
 
 var p1 = require('./parser/portal_pravnych_predpisov_parser.js')(FESIO.portal_pravnych_predpisov);
 
