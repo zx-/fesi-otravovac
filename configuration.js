d4e33b132c358553a23ffc8b3fc046b110c83369
zx- <robertcuprik@hotmail.com>
2015-12-19 01:04:49 +0100

    Adds parsers config and test main

--- configuration.js
+++ configuration.js
@@ -1,3 +1,11 @@
 /**
  * Created by z on 18.12.2015.
  */
+
+var FESIO = module.exports;
+
+FESIO.portal_pravnych_predpisov = {
+    base_url: 'https://lt.justice.gov.sk',
+    parse_url: 'https://lt.justice.gov.sk/Public/AllMaterialsList.aspx?instEID=-1&lCategories=1048574&langEID=1'
+};
+
--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 /**
  * Created by z on 18.12.2015.
  */
+
+var FESIO = require ('./configuration.js');
+
+var p1 = require('./parser/portal_pravnych_predpisov_parser.js')(FESIO.portal_pravnych_predpisov);
+
+
+p1.parse(function(res){
+
+    console.log(res);
+
+});
