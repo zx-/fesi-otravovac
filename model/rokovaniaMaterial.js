98152854605ea3f2c0cd11dd6344e7ca9e54d255
zx- <robertcuprik@hotmail.com>
2015-12-24 16:16:41 +0100

    adds rokovania material

---
+++ model/rokovaniaMaterial.js
@@ -0,0 +1,37 @@
+/**
+ * Created by z on 24.12.2015.
+ */
+var Sequelize = require('sequelize');
+
+module.exports = function ( sequelize ) {
+
+    var rokovaniaMaterial = sequelize.define('rokovaniamaterial', {
+        number:{
+            type: Sequelize.INTEGER
+        },
+        rokovanie: {
+            type: Sequelize.TEXT
+        },
+        date: {
+            type: Sequelize.DATE
+        },
+        materials:{
+            type: Sequelize.TEXT
+        },
+        recordings:{
+            type: Sequelize.TEXT
+        },
+        resolution:{
+            type: Sequelize.TEXT
+        },
+        communique:{
+            type: Sequelize.TEXT
+        }
+
+    }, {
+        freezeTableName: true // Model tableName will be the same as the model name
+    });
+
+    return rokovaniaMaterial;
+
+};
