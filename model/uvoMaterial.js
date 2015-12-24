4eb18ae855f6893627a6f4c3f185afa79852956d
zx- <robertcuprik@hotmail.com>
2015-12-24 16:11:16 +0100

    adds uvoMaterial

---
+++ model/uvoMaterial.js
@@ -0,0 +1,31 @@
+/**
+ * Created by z on 24.12.2015.
+ */
+var Sequelize = require('sequelize');
+
+module.exports = function ( sequelize ) {
+
+    var uvoMaterial = sequelize.define('uvomaterial', {
+        name: {
+            type: Sequelize.TEXT
+        },
+        date: {
+            type: Sequelize.DATE
+        },
+        href:{
+            type: Sequelize.TEXT
+        },
+        description:{
+            type: Sequelize.TEXT
+        },
+        type:{
+            type: Sequelize.INTEGER
+        }
+
+    }, {
+        freezeTableName: true // Model tableName will be the same as the model name
+    });
+
+    return uvoMaterial;
+
+};
