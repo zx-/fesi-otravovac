c872bedf6367d9de8305e2830118614f88b30153
zx- <robertcuprik@hotmail.com>
2015-12-29 17:15:30 +0100

    Adds date parsing to fronend

--- frontend/ang-app/controllers/home.js
+++ frontend/ang-app/controllers/home.js
@@ -8,17 +8,31 @@ controllers.controller('home',[
 
     function ( $scope, Restangular ) {
 
+        function createDate(o){
+
+            o.date = new Date(o.date);
+            o.date.setTime( o.date.getTime() + 60*60*1000 );
+            return o;
+
+        }
+
+        $scope.getDateString = function (date) {
+
+            return date.getDate()+'. '+(date.getMonth()+1)+'. '+date.getFullYear();
+
+        };
+
         $scope.pppMaterials = [];
 
         Restangular.all('pppMaterial').getList().then(
-            function(resp){ $scope.pppMaterials = resp;},
+            function(resp){ $scope.pppMaterials = resp.map(createDate);},
             function(resp){ console.log(resp);}
         );
 
         $scope.nkuMaterials = [];
 
         Restangular.all('nkuMaterial').getList().then(
-            function(resp){ $scope.nkuMaterials = resp;},
+            function(resp){ $scope.nkuMaterials = resp.map(createDate);},
             function(resp){ console.log(resp);}
         );
 
